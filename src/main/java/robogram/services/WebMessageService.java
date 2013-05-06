3530add403499f7a750627d5d78f14b580d540f9
Jeremy Moore <jmoore@twilio.com>
2013-05-05 21:19:40 -0700

    add twilio service

---
+++ src/main/java/robogram/services/WebMessageService.java
@@ -0,0 +1,52 @@
+package robogram.services;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.CallFactory;
+import com.twilio.sdk.resource.instance.Account;
+
+@Path("/websend")
+@Produces(MediaType.APPLICATION_XML)
+public class WebMessageService {
+
+	private static Logger logger = Logger.getLogger(WebMessageService.class
+			.getName());
+	private static final String HOSTNAME = System.getenv("HOSTNAME");
+
+	@POST
+	public Response handleWebMessage(@QueryParam("number") String number,
+			@QueryParam("message") String message) {
+		TwilioRestClient twilio = new TwilioRestClient(
+				System.getenv("TWILIO_ACCOUNT_SID"),
+				System.getenv("TWILIO_AUTH_TOKEN"));
+		Account account = twilio.getAccount();
+		CallFactory callFactory = account.getCallFactory();
+		Map<String, String> callParams = new HashMap<String, String>();
+		callParams.put("To", number);
+		callParams.put("From", System.getenv("TWILIO_FROM"));
+		callParams.put("Method", "GET");
+		UriBuilder uriBuilder = UriBuilder
+				.fromPath("http://{hostname}/services/robocall");
+		uriBuilder.queryParam("message", message);
+		callParams.put("Url", uriBuilder.build(HOSTNAME).toString());
+		logger.info("Creating call with url: " + callParams.get("Url"));
+		try {
+			callFactory.create(callParams);
+		} catch (TwilioRestException e) {
+			return Response.serverError().build();
+		}
+		return Response.ok().build();
+	}
+}
